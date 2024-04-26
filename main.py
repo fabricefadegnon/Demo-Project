71ce5ff127969421e82b7d5980d4a34455358239
fabricefadegnon <FabriceFadegnon@zeleus.com>
2024-04-26 13:58:18 +0200

    Initial commit

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ Result_not = not is_teacher
 print(Result_And)
 print(Result_or)
 print(Result_not)
+## Variables
+message = "And now for something completely different"
+n = 17
+pi = 3.1415926535897931
+print(n)
+print(pi)
+## Type of Variables
+type(message)
+type(n)
+type(pi)
